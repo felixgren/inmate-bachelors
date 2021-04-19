cd8c6b1a3c4c93ec4d45302a49bdf2d895ed8d2a
felixgren <33127919+felixgren@users.noreply.github.com>
2021-04-20 01:53:16 +0200

    Added header, footer & themes
    + other minor changes like favicon

---
+++ src/components/TestProfile2.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Testprofile2 = () => {
+  return (
+    <div className="flex flex-col items-center justify-center px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:pt-32 md:px-0">
+      <div className="flex flex-col items-center max-w-2xl md:px-8">
+      </div>
+      <img
+        src="https://imgstore.jailbase.com/arrested/az-mcso/2020-10-28/kalani-smith-t662411.pic1.jpg"
+        className="w-full max-w-screen-sm mx-auto rounded shadow-2xl md:w-auto lg:max-w-screen-md"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Testprofile2;
