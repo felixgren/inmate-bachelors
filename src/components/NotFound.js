ea323aae57ad3d43354ed89b34993977000c0685
felixgren <33127919+felixgren@users.noreply.github.com>
2021-04-20 06:21:59 +0200

    Fixed netlify routes & minor 404 update

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,11 +4,11 @@ import goodlad from "../videos/goodlad404.mp4";
 
 const NotFound = (props) => {
   return (
-    <div class="bg-red-500 h-screen">
-      <h1 class="font-bold text-center text-white">
+    <div class="bg-red-500 h-auto w-screen">
+      <h1 class="font-bold text-center m-4 p-4 mt-12 text-white">
         THERE'S NOTHING HERE, 404!!!11
       </h1>
-      <wrapper class="w-screen h-screen bg-blue-500 h-full">
+      <wrapper class="w-screen h-screen bg-blue-500">
         <video autoPlay loop>
           <source src={goodlad} type="video/mp4" />
         </video>
