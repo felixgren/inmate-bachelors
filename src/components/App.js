26ebcae7eb6df05fb1dcc6cafb773cdfdacc571a
felixgren <33127919+felixgren@users.noreply.github.com>
2021-04-20 02:49:27 +0200

    Fixed cors & started swipe functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,92 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import Test from './Test';
-import Header from './Header';
-import TestProfile2 from './TestProfile2';
-import Footer from './Footer';
-
-// async function fetchBachelors() {
-//     const response = await fetch('http://www.JailBase.com/api/1/recent/?source_id=az-mcso', {
-//         method: 'GET',
-//         mode: 'no-cors',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//     })
-//     // .then(response => response.json())
-//     // .then((result) => {
-//     //     console.log(result);
-//     //     setBachelors(result);
-//     //     setLoaded(true);
-//     // },
-//     // (error) => setLoaded(false))
-//     let test = response.json();
-//     console.log(test)
-
-//     return test;
-
-// }
+import React, { useState, useEffect } from "react";
+import Test from "./Test";
+import TinderCard from "react-tinder-card";
+import Header from "./Header";
+import TestProfile2 from "./TestProfile2";
+import Footer from "./Footer";
 
 const App = () => {
-  const [bachelors, setBachelors] = useState(['']);
+  const [bachelors, setBachelors] = useState([""]);
   const [isLoaded, setLoaded] = useState(false);
-  
+
   const fetchBachelors = async () => {
     // const data = await fetch("https://www.breakingbadapi.com/api/characters?name=walter")
-    const data = await fetch("http://www.JailBase.com/api/1/recent/?source_id=az-mcso", {
-      headers: {'Access-Control-Allow-Origin': '*'}
+    const data = await fetch("/api/1/recent/?source_id=az-mcso", {
+      headers: { "Access-Control-Allow-Origin": "*" },
     })
-    // const data = await fetch("https://www.jailbase.com/api/1/search/?source_id=az-mcso&last_name=smith")
-    // .then(response => console.log(response))
-    .then(response => response.json())
-    .then(response => {
-      console.log(response.records);
-    },
-    (error) => console.log(error))
+      // const data = await fetch("https://www.jailbase.com/api/1/search/?source_id=az-mcso&last_name=smith")
+      // .then(response => console.log(response))
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.records);
+      });
 
+    console.log(data);
+    // setBachelors(result);
+    // setLoaded(true);
   };
 
-  // fetchBachelors();
-
-
-
-
   useEffect(() => {
-
-    console.log('hej')
     fetchBachelors();
-
-    // const fetchBachelors = async () => {
-    //   const testing = await fetch('http://www.JailBase.com/api/1/recent/?source_id=az-mcso', {
-    //       method: 'GET',
-    //       mode: 'no-cors',
-    //       headers: {
-    //         'Content-Type': 'application/json'
-    //       },
-    //   })
-    //   .then(response => response.json())
-    //   .then((result) => {
-    //       console.log('hej');
-    //       console.log(result);
-    //       setBachelors(result);
-    //       setLoaded(true);
-    //   },
-    //   (error) => console.log(error))
-    // };
-
-    // fetchBachelors();
-
   }, []);
 
-  return (
+  const onSwipe = (direction) => {
+    console.log("You swiped: " + direction);
+  };
 
+  const onCardLeftScreen = (myIdentifier) => {
+    console.log(myIdentifier + " left the screen");
+  };
+
+  return (
     <div>
+      <TinderCard
+        onSwipe={onSwipe}
+        onCardLeftScreen={() => onCardLeftScreen("fooBar")}
+        preventSwipe={["right", "left"]}
+      >
+        <img
+          src="https://imgstore.jailbase.com/arrested/az-mcso/2020-10-28/kalani-smith-t662411.pic1.jpg"
+          className="w-full max-w-screen-sm mx-auto rounded shadow-2xl md:w-auto lg:max-w-screen-md"
+          alt=""
+        />
+      </TinderCard>
+
       <Header />
       <TestProfile2 />
       {/* <Test /> */}
       <Footer />
     </div>
-
   );
-}
+};
 
 export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,21 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const Test = () => {
+  return (
+    <div>
+        
+      <p>hej hej</p>
+
+      <button
+        type="button"
+        class="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-24 m-auto transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+      >
+        Valider
+      </button>
+
+      
+    </div>
+  );
+};
+
+export default Test;
